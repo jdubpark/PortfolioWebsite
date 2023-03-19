8e20db93f4747575b6865ad0dc9cc7b662fafedb
Jongwon Park <jwparktom@gmail.com>
2023-03-18 19:00:05 -0500

    Text data updates

--- src/data/about.ts
+++ src/data/about.ts
@@ -16,31 +16,31 @@ const shortWords: AboutShortWordsProps[] = [
 	{
 		title: 'Intro',
 		content: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			'I\'m a highly motivated student & learner, actively developing in the blockchain ecosystem. I\'ve spent the last two years developing on-chain dApps in Solidity and Rust and many more years on JavaScript/TypeScript for fullstack',
 		],
 		// My first code was written in a elementary school class on my old, loud HP laptop. Fascinated by the basic HTML website flashing my name (http://localhost:3000) and enchanted by the speed of finger-to-screen reflection, I continued to code day and night.
 	},
 	{
 		title: 'Interests',
 		content: [
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-			'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+			'Within blockchain, I extensively research in DeFi and build prop trading tools around it for a trading firm I work at. I\'m also diving into MEV, actively writing tools for JIT liquidity and other less-invasive actions. I\'ve recently got into the block building world of Ethereum 2.0.',
+			'I\'m enthralled by the beauty of math; I love to read academic papers on derivatives and quantitative analysis of various mechanisms within DeFi and blockchain.',
+		],
+	},
+	{
+		title: 'Thesis',
+		content: [
+			'My main crypto thesis for 2023 is alt Layer-1 Multi-chain. I\'m also concerned with the security risks of DeFi and fond of formal verification.',
+			'I\'m also fond of activities around advanced cryptographic primitives in DeFi. I believe that AI/ML will need to marry with blockchain to provide human-friendly UI/UX to onboard the next billion users.'
+		],
+	},
+	{
+		title: 'Past',
+		content: [
+			'I\'ve extensively explored NLP and LLMs, e.g. self-attention, Transformers, and BERT.',
 		],
 	},
 ]
-
-// <p>
-// 	I&apos;m a highly motivated student & learner, actively developing in the blockchain ecosystem. I&apos;ve spent the last two years developing on-chain dApps in Solidity and Rust. With my extensive TypeScript/React background of four years, I&apos; comfortable writing Web3 applications.
-// </p>
-// <p>
-// 	Within blockchain, I extensively research in DeFi and build prop trading tools around it for a trading firm I work at. I&apos;m also diving into MEV, actively writing tools for JIT liquidity and other less-invasive actions. I&apos;ve recently got into the block building world of Ethereum 2.0.
-// </p>
-// <p>
-// 	I&apos;m also an avid math-er, focusing on derivatives and quantitative analysis of MM and lending/hedging markets.
-// </p>
-// <p>
-// 	Though I started out as an NLP enthusiast, I&apos;ve now pivoted to combine NLP with blockchain to develop advanced tools around cross-chain DeFi.
-// </p>
 
 export default {
 	profileShorts,
--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -1,3 +1,5 @@
+import Stack from '@mui/material/Stack'
+import Typography from '@mui/material/Typography'
 import React from 'react'
 
 import beetectLogo from '../assets/logo/beetect.png'
@@ -11,6 +13,21 @@ import zkKYCSBTLogo from 'url:../assets/logo/zkKYCSBT.svg'
 
 const imageStyle = { height: '100%', width: 'auto', maxHeight: 'inherit', margin: '0 auto' }
 
+function BasicTextBlocks({ texts }: { texts: string[] }) {
+	return (
+		<Stack
+			direction="column"
+			justifyContent="flex-start"
+			alignItems="flex-start"
+			spacing={2}
+			width="100%"
+			maxWidth={{ md: '700px' }}
+		>
+			{texts.map((text, idx) => (<Typography key={idx} variant="body1">{text}</Typography>))}
+		</Stack>
+	)
+}
+
 const projectItems: ProjectItemProps[] = [
 	{
 		name: 'ChatGTP',
@@ -18,14 +35,26 @@ const projectItems: ProjectItemProps[] = [
 		fromTime: 'Mar 2023',
 		toTime: '',
 		github: 'https://github.com/xChatGTP',
+		youtube: 'https://www.youtube.com/watch?v=RHC7V6ejL2g',
 		webMain: 'https://app.buidlbox.io/projects/chatgtp',
+		blocks: <BasicTextBlocks texts={[
+			'Chat Generic Transaction Passing (ChatGTP) is a PoC on Axelar for batching and sending multiple multi-chain transactions in a single transaction, constructed using the human-friendly ChatGPT frontend clone.',
+			'Won: 2nd place in Axelar Multi-chain bounty @ ETH Denver',
+			'Won: 2nd place in Moonbeam GMP bounty @ ETH Denver',
+		]} />,
 	},
 	{
-		name: 'Nordle (NFT\u00a0Wordle)', // Random Generative NFT
+		name: 'Nordle', // Random Generative NFT
 		logo: <img src={nordleLogo} alt="Nordle" style={imageStyle} />,
 		fromTime: 'Nov 2022',
 		toTime: '',
 		github: 'https://github.com/Nordle-404',
+		youtube: 'https://www.youtube.com/watch?v=C2dKyKeqq-0',
+		blocks: <BasicTextBlocks texts={[
+			'NFT Wordle (Nordle) is a tool to create and concatenate random generative NFTs. In a click, users can collect a single-word NFT, which they can combine into longer phrases to create more expressive NFTs.',
+			'It uses DALLE-2 to generate an image from a given word/phrase. It uses Chainlink VRF and Any API to generate a random word and call the custom DALLE-2 API.',
+			'Won: Best Use of Chainlink @ HackATL',
+		]} />,
 	},
 	{
 		name: 'xHolas',
@@ -33,6 +62,11 @@ const projectItems: ProjectItemProps[] = [
 		fromTime: 'Oct 2022',
 		toTime: '',
 		github: 'https://github.com/xHolas-Pit',
+		webMain: 'https://www.loom.com/share/7691e569d6f74a098961e89daddb0e87',
+		blocks: <BasicTextBlocks texts={[
+			'xHolas is a PoC on Wormhole for combing various multi-chain DeFi transactions into a single transaction, which users can execute and sell on the secondary market as an NFT.',
+			'Won: Best Use of Wormhole @ xHack at the Pit',
+		]} />,
 	},
 	{
 		name: 'xName',
@@ -40,6 +74,10 @@ const projectItems: ProjectItemProps[] = [
 		fromTime: 'Oct 2022',
 		toTime: '',
 		github: 'https://github.com/xAbelian-Pit/xName',
+		youtube: 'https://www.youtube.com/watch?v=f18dMFwR5wE',
+		blocks: <BasicTextBlocks texts={[
+			'xName is a multi-chain Domain Name Service (DNS) built on Wormhole. It uses xAbelian as the base layer to guarantee domain name synchronization across chains.',
+		]} />,
 	},
 	{
 		name: 'xAbelian',
@@ -47,20 +85,35 @@ const projectItems: ProjectItemProps[] = [
 		fromTime: 'Oct 2022',
 		toTime: '',
 		github: 'https://github.com/xAbelian-Pit/Abelian',
+		blocks: <BasicTextBlocks texts={[
+			'xAbelian is a generic, dynamic NFT anchored on Klaytn using Wormhole. It uses a mesh network architecture to guarantee state synchronization across chains supported by Wormhole.',
+			'Users can use xAbelian to update and prove their data ownership on other chains through NFT metadata. ',
+			'Won: 1st place in Klaytn bounty @ xHack at the Pit',
+		]} />,
 	},
 	{
-		name: 'zkSNARK KYC Soulbound',
+		name: 'zk KYC SBT',
 		logo: <img src={zkKYCSBTLogo} alt="zkKYCSBT" style={imageStyle} />,
 		fromTime: 'Sep 2022',
 		toTime: '',
 		github: 'https://github.com/ETHNYC-ZK-KYC-Soulbound',
+		blocks: <BasicTextBlocks texts={[
+			'zkKYC SBT is a PoC for zk-SNARK based Soulbound token (SBT) storing KYC data. As a privacy-preserving tool, it stores zk-SNARK to prove the KYC data is valid and exists as a Sybil-resistant SBT.',
+			'Won: 1st place in WallectConnect DeFi bounty @ ETH NewYork',
+		]} />,
 	},
 	{
 		name: 'Continual BERT',
 		logo: <img src={continualBERTLogo} alt="Continual BERT" style={imageStyle} />,
-		fromTime: 'Dec 2020',
-		toTime: 'Mar 2021',
+		fromTime: 'Apr',
+		toTime: 'Dec 2020',
 		github: 'https://github.com/jdubpark/continual-bert',
+		webMain: 'https://arxiv.org/abs/2007.03405',
+		blocks: <BasicTextBlocks texts={[
+			'A novel language model to provide quality summaries on influx of scientific papers by mimicking the brain\'s day-and-night learning cycle.',
+			'Heavily inspired by online Elastic Weight Loss (online), this novel model learns and consolidates information to mimic memory while minimizing catastrophic forgetting.',
+			'It utilizes two separate BERT models with layer-wise connections and deploys an alternating training process, which resembles the brain\'s learning during the day and consolidation of knowledge while sleeping.',
+		]} />,
 	},
 	{
 		name: 'Beetect',
@@ -68,7 +121,24 @@ const projectItems: ProjectItemProps[] = [
 		fromTime: 'Jan',
 		toTime: 'May 2021',
 		github: 'https://github.com/jdubpark/beetect',
+		webMain: 'https://drive.google.com/file/d/1c2-Ci1co7iXfoEQjH9rsZZmOse3J_fkI/view?usp=sharing',
+		blocks: <BasicTextBlocks texts={[
+			'An experiment on training and detecting bees using Faster R-CNN in a compute-limited environment. It uses a custom curated dataset of 1,000+ bee images and an efficient model with a ResNet-50 backbone.',
+		]} />,
 	}
 ]
 
 export default projectItems
+
+{/* <Typography
+	variant="body2"
+	overflow="hidden"
+	textOverflow="ellipsis"
+	display="-webkit-box"
+	sx={{
+		WebkitLineClamp: '3', // 3-lines ellipsis
+		WebkitBoxOrient: 'vertical',
+	}}
+>
+	This is some description that works better than Lorem Ipsum to create content that looks legit when skimping over, as it is more natural than the repeated use of same words and letters.
+</Typography> */}
