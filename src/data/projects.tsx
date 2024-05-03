ab5c242f74adf12403c99b066a6f08712050d31a
Jongwon Park <jwparktom@gmail.com>
2024-05-03 00:20:46 -0700

    feat: more projects

--- src/data/projects.tsx
+++ src/data/projects.tsx
@@ -11,6 +11,10 @@ import xHolasLogo from 'url:../assets/logo/xHolas.svg'
 import xNameLogo from 'url:../assets/logo/xName.svg'
 import zkKYCSBTLogo from 'url:../assets/logo/zkKYCSBT.svg'
 import uiucBlueLogo from 'url:../assets/logo/uiucBlue.png'
+import initMailLogo from 'url:../assets/logo/initMail.png'
+import cladLogo from 'url:../assets/logo/clad.png'
+import interbeamLogo from 'url:../assets/logo/interbeam.png'
+import wordsRealmPGF from 'url:../assets/logo/wordsRealmPGF.png'
 
 const imageStyle = { height: '100%', width: 'auto', maxHeight: 'inherit', margin: '0 auto' }
 
@@ -31,8 +35,76 @@ function BasicTextBlocks({ texts }: { texts: string[] }) {
 
 const projectItems: ProjectItemProps[] = [
 	{
+		name: 'Words Realm PGF',
+		logo: <img src={wordsRealmPGF} alt="WordsRealmPGF" style={imageStyle} />,
+		fromTime: 'Nov 2023',
+		toTime: '',
+		github: 'https://github.com/jdubpark/WordRealmForGood',
+		youtube: 'https://www.youtube.com/watch?v=4kVkL8W3j-o',
+		webMain: 'https://ethglobal.com/showcase/pro-activepgf-hnvax',
+		blocks: <BasicTextBlocks texts={[
+			'Fun generative AI-based word-assembling game that collects minting fees for charity. Players can mint words and assemble them into phrases, from which DALLE-3 generates unique images. All minting fees are donated to charity with automatic weather-based fund dispersion onchain.',
+			'Won: Worldcoin Pool Prize @ ETH Istanbul 2023'
+		]} />,
+	},
+	{
+		name: 'Interbeam',
+		logo: <img src={interbeamLogo} alt="Interbeam" style={imageStyle} />,
+		fromTime: 'Oct 2023',
+		toTime: '',
+		github: 'https://github.com/Interbeam/contracts',
+		youtube: 'https://www.youtube.com/watch?v=4k1fgRn6DsM',
+		webMain: 'https://www.youtube.com/watch?v=HFYvMMt491E',
+		blocks: <BasicTextBlocks texts={[
+			'Simple DeFi Migration from EVM to Solana. With a few clicks, users can move DeFi liquidity from Ethereum and other chains to Solana. Aave to Marginfi, Uniswap to Orca. Say it, we integrate into at most four clicks on Interbeam.',
+			'Submission @ Solana Hyperdrive hackathon',
+			'Pitch deck: https://pitch.com/v/interbeam-fs9gjq'
+		]} />,
+	},
+	{
+		name: 'Aucentive',
+		logo: <img src={initMailLogo} alt="Aucentive" style={imageStyle} />,
+		fromTime: 'Sep 2023',
+		toTime: '',
+		github: 'https://github.com/orgs/Aucentive/repositories',
+		youtube: 'https://www.youtube.com/watch?v=SlkQK6EpAgo',
+		webMain: 'https://ethglobal.com/showcase/aucentive-xjk04',
+		blocks: <BasicTextBlocks texts={[
+			'Zapier for Incentives. Automated incentive protocol on Base with seamless Web2 integration with Account Abstraction.',
+			'Won: UMA Pool Prize @ ETH NYC 2023'
+		]} />,
+	},
+	{
+		name: 'Initmail',
+		logo: <img src={initMailLogo} alt="Initmail" style={imageStyle} />,
+		fromTime: 'Sep 2023',
+		toTime: '',
+		github: 'https://github.com/orgs/InitMail/repositories',
+		youtube: 'https://www.youtube.com/watch?v=XRwTWRzfL_c',
+		webMain: 'https://taikai.network/ethchicago/hackathons/ETHChicagoHackathon2023/projects/clmmbkpth04zdwn019c0yzbk3/idea',
+		blocks: <BasicTextBlocks texts={[
+			"Senders can attach \"value\" to their emails. It's eye-grabbing for recipients for two reasons: generates revenue, signifies seriousness, and shows consideration. If someone's willing to spend $100 to write an email, maybe it's really important?",
+			"Won: Best Integration of Wormhole @ ETH Chicago",
+			"Won: Best Use of Scaffold-ETH @ ETH Chicago",
+		]} />,
+	},
+	{
+		name: 'CLAD',
+		logo: <img src={cladLogo} alt="CLAD" style={imageStyle} />,
+		fromTime: 'May 2023',
+		toTime: 'Aug 2023',
+		github: 'https://github.com/solana-labs/interns-codehub/tree/master/infinity-pools',
+		youtube: 'https://drive.google.com/file/d/1l_34cLn6WM_PQosoQm1ubguxwgmdBIZ0/view?resourcekey',
+		webMain: 'https://docs.google.com/presentation/d/1ZRLwpWqIjuvsBblvYzGg3_AQoBdhj_nXJz8J1v4rklk/edit?usp=sharing',
+		blocks: <BasicTextBlocks texts={[
+			"Internship project at Solana Labs.",
+			"Concentrated Liquidity AMM Derivatives, or CLAD, is an open-source implementation of a non-custodial decentralized exchange that supports leveraged trading in any asset using concentrated liquidity positions as counter-parties.",
+			"Basically simplified InfinityPools on Solana with code modified from Orca Whirlpool."
+		]} />,
+	},
+	{
 		name: 'No-need DFS',
-		logo: <img src={uiucBlueLogo} alt="ChatGTP" style={imageStyle} />,
+		logo: <img src={uiucBlueLogo} alt="No-need DFS" style={imageStyle} />,
 		fromTime: 'May 2023',
 		toTime: '',
 		github: 'https://gist.github.com/jdubpark/1be5713022eee374dae25b41365e0890',
